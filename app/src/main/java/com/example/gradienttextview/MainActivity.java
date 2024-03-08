dd0513b58b0b2bf52014df878bb26be9785e15c0
Amisha0971 <amishaninawe971@gmail.com>
2024-03-08 22:20:37 +0530

    first commit

---
+++ app/src/main/java/com/example/gradienttextview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.gradienttextview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
