f31fd12022ebcac362352fe65743d65d9a5cc22c
Amisha0971 <amishaninawe971@gmail.com>
2024-03-09 16:19:07 +0530

    second commit

--- app/src/main/java/com/example/gradienttextview/MainActivity.java
+++ app/src/main/java/com/example/gradienttextview/MainActivity.java
@@ -2,18 +2,33 @@ package com.example.gradienttextview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
     public WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = (WebView) findViewById(R.id.webview);
+        webView = findViewById(R.id.webview);
         webView.loadUrl("file:///android_asset/index.html");
+
+        // Show gradient for 1 minute
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start SecondActivity
+                Intent intent = new Intent(MainActivity.this, secondActivity.class);
+                startActivity(intent);
+                finish(); // Optional, if you don't want to keep MainActivity in the back stack
+            }
+        }, 30000); // 30000 milliseconds = 30 sec
     }
 }
